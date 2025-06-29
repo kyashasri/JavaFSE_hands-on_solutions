b86d99a90fdc125e772c3072d2708ec1d2641e6a
kyashasri <168709831+kyashasri@users.noreply.github.com>
2025-06-29 19:46:39 +0530

    created BookService.java

--- Week 3/Spring Core and Maven/01_Exercise 1: Configuring a Basic Spring Application/Code/BookService.java
+++ Week 3/Spring Core and Maven/01_Exercise 1: Configuring a Basic Spring Application/Code/BookService.java
@@ -5,7 +5,6 @@ import com.library.repository.BookRepository;
 public class BookService {
     private BookRepository bookRepository;
 
-    // Setter for dependency injection
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
