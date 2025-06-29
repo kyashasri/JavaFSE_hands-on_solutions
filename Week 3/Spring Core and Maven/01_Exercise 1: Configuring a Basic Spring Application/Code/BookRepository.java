9920b799598fbcdea481d1d5c7bc49dc6d4fc887
kyashasri <168709831+kyashasri@users.noreply.github.com>
2025-06-29 19:45:49 +0530

    created BookRepository.java

--- Week 3/Spring Core and Maven/01_Exercise 1: Configuring a Basic Spring Application/Code/BookRepository.java
+++ Week 3/Spring Core and Maven/01_Exercise 1: Configuring a Basic Spring Application/Code/BookRepository.java
@@ -2,6 +2,6 @@ package com.library.repository;
 
 public class BookRepository {
     public void getBookData() {
-        System.out.println("Fetching book details from repository");
+        System.out.println("Fetching book data from repository...");
     }
 }
